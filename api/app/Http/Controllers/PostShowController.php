f260a6387b224be3fe4f46a942e58688c0d1e0a3
tylerlouwerse <tyler@ezmech.ca>
2023-06-19 20:56:19 -0400

    fetch single post

---
+++ api/app/Http/Controllers/PostShowController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Http\Resources\PostResource;
+
+class PostShowController extends Controller
+{
+    public function __invoke(Post $post)
+    {
+        // authorize
+
+        return new PostResource($post);
+    }
+}
